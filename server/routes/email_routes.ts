14ee0c6cfdaf6e5a9f157bfc59ee150dd7449fe0
Richard Lefevre <rlefevre.6505@gmail.com>
2026-06-18 13:25:02 +0100

    changed resend send email

--- server/routes/email_routes.ts
+++ server/routes/email_routes.ts
@@ -11,7 +11,8 @@ emailRouter.post("/support", async (req: Request, res: Response) => {
   try {
     const { name, email, message } = req.body;
     const data = await resend.emails.send({
-      from: `${process.env.CUSTOM_DOMAIN}`,
+      // from: `${process.env.CUSTOM_DOMAIN}`,
+      from: `onboarding@resend.dev`,
       to: `${process.env.SUPPORT_EMAIL}`,
       subject: "Tom The Shop Support",
       html: `
